e89ebc12b26355d41a04b4f3903af38818d02150
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-03 16:31:24 +0530

    Time: 4 ms (85.25%), Space: 14.1 MB (42.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,49 +1,30 @@
-void __print(int x) { cout << x; }
-void __print(long x) { cerr << x; }
-void __print(long long x) { cerr << x; }
-void __print(unsigned x) { cerr << x; }
-void __print(unsigned long x) { cerr << x; }
-void __print(unsigned long long x) { cerr << x; }
-void __print(float x) { cerr << x; }
-void __print(double x) { cerr << x; }
-void __print(long double x) { cerr << x; }
-void __print(char x) { cerr << '\'' << x << '\''; }
-void __print(const char *x) { cerr << '\"' << x << '\"'; }
-void __print(const string &x) { cout << '\"' << x << '\"'; }
-void __print(bool x) { cout << (x ? "true" : "false"); }
-template <typename T, typename V>
-void __print(const pair<T, V> &x){cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
-template <typename T>
-void __print(const T &x){int f = 0;cerr << '{';for (auto &i : x)cerr << (f++ ? "," : ""), __print(i);cout << "}";}
-void _print() { cout << "]\n"; }
-template <typename T, typename... V>
-void _print(T t, V... v)
-{__print(t);if (sizeof...(v))cerr << ", ";_print(v...);}
-
-#define dbg(x...)               \
-  cout << "[" << #x << "] = ["; \
-  _print(x)
 
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        map<int,int> dp1;
-        dp1[0]=1;
-        for(int i=0;i<nums.size();i++){
-            map<int,int> dp2;
-            for(auto prev:dp1){
-                int now=prev.first+nums[i];
-                dp2[now]=dp2[now]+prev.second;
-                now=prev.first-nums[i];
-                dp2[now]=dp2[now]+prev.second;
-            }
-            
-            cout<<'\n';
-            dp1=dp2;
-            // dbg(dp1);
+        target=abs(target);
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum<target || (sum+target)&1){
+            return 0;
         }
-        int ans=dp1[target];
-        return ans;
+        return countSubarrayofSum(nums,(sum+target)>>1);
+    }
+    int countSubarrayofSum(vector<int>& nums,int target){
+        int n=nums.size();
         
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                if(j>=nums[i-1]){
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+                // cout<<"#"<<i<<"#"<<j<<dp[i][j]<<'\n';
+            }
+        }
+        return dp[n][target];
     }
 };
