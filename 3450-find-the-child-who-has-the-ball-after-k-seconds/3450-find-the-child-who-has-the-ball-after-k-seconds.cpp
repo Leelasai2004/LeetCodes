450136d284037eda01a34e4c75d4d31ad17e3a88
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-09 08:04:55 +0530

    Time: 0 ms (100%), Space: 7.5 MB (83.33%) - LeetHub

---
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfChild(int n, int k) {
+        int position = 0;
+        int direction = 1;
+
+        for (int i = 0; i < k; ++i) {
+            position += direction;
+
+            if (position == 0) {
+                direction = 1;
+            } else if (position == n - 1) {
+                direction = -1;
+            }
+        }
+
+        return position;
+    }
+};
