0d2f397f6ca927cc2a91dff4eb5ec090155c6e70
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-10 17:26:04 +0530

    Time: 2 ms (48.33%), Space: 10.1 MB (78.64%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,8 +1,36 @@
 class Solution {
 public:
+    void heapify(vector<int> &v,int n,int i){
+        int smallest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<n && v[left]<v[smallest])
+        {
+            smallest=left;
+        }
+        if(right<n && v[right]<v[smallest])
+        {
+            smallest=right;
+        }
+        if(smallest!=i){
+            swap(v[i],v[smallest]);
+            heapify(v,n,smallest);
+        }
+    }
+    void heapsort(vector<int> &arr){
+        int n=arr.size();
+        for(int i=n/2-1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+        for(int i=n-1;i>=0;i--){
+            swap(arr[0],arr[i]);
+            heapify(arr,i,0);
+        }
+        reverse(arr.begin(),arr.end());
+    }
     int heightChecker(vector<int>& h) {
         vector<int> c=h;
-        sort(c.begin(),c.end());
+        heapsort(c);
         int cnt=0;
         for(int i=0;i<c.size();i++){
             if(c[i]!=h[i]){
