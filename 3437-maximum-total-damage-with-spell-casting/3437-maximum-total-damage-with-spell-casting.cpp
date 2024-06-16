8bc0f22d69f22ad0ed8d63214fe64b25438a0637
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-16 19:21:52 +0530

    Time: 235 ms (100%), Space: 137.7 MB (100%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,24 +1,27 @@
-
 class Solution {
 public:
-    long long maximumTotalDamage(vector<int>& p) {
-        map<long long,long long> mp;
-        long long maxx=0;
-        for(int i=0;i<p.size();i++){
-            mp[p[i]]+=p[i];
-        }
-        map<long long,long long> dp;
-        queue<pair<long long ,long long>> q;
-        long long ans=0;
-        for(auto m:mp){
-            while(!q.empty() && q.front().first<m.first-2){
-                maxx=max(maxx,q.front().second);
-                q.pop();
+    long long maximumTotalDamage(vector<int>& power) {
+        sort(power.begin(),power.end());
+        int n=power.size();
+        vector<long long> dp(n,0);
+        int i=0,j=0;
+         long long mx=0;
+         int prev=0;
+        for(;i<n;i++){
+            while(power[j]<power[i]-2){
+                mx=max(mx,dp[j]);
+                j++;
             }
-            dp[m.first]=m.second+maxx;
-            ans=max(ans,dp[m.first]);
-            q.push({m.first,ans});
+            long long cnt=1;
+            if(i) prev=i-1;
+            while(i<n-1 && power[i]==power[i+1]){
+                cnt++;
+                i++;
+            }
+            // cout<<power[i]<<" "<<cnt<<'\n';
+            dp[i]=mx+1LL*cnt*power[i];
+            if(i && prev) dp[i]=max(dp[prev],dp[i]);
         }
-        return ans;
+        return dp[n-1];
     }
 };
