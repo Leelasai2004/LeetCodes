08ac15fcd86cb4498899de7dd7fff4af280f3e89
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-20 15:11:13 +0530

    Time: 105 ms (83.85%), Space: 69 MB (65.57%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    // Return the number of maximum bouquets that can be made on day mid.
-    int getNumOfBouquets(vector<int>& bloomDay, int mid, int k) {
-        int numOfBouquets = 0;
-
-        int count = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            // If the flower is bloomed, add to the set. Else reset the count.
-            if (bloomDay[i] <= mid) {
-                count++;
-            } else {
-                count = 0;
+    int minDays(vector<int>& b, int m, int k) {
+        int l=0,r=*max_element(b.begin(),b.end());
+        int ans=-1;
+        while(l<=r){
+            int mid=l-(l-r)/2;
+            int res=0,cnt=0;
+            for(auto n:b ){
+                if(n<=mid){
+                    cnt++;
+                }
+                else
+                {
+                    res+=(cnt/k);
+                    cnt=0;
+                }
+            }
+            res+=(cnt/k);
+            // cout<<mid<<" "<<l<<" "<<r<<" "<<res<<'\n';
+            if(res>=m){
+                ans=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
             }
 
-            if (count == k) {
-                numOfBouquets++;
-                count = 0;
-            }
         }
-
-        return numOfBouquets;
-    }
-
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int start = 0;
-        int end = 0;
-        for (int day : bloomDay) {
-            end = max(end, day);
-        }
-
-        int minDays = -1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
-                minDays = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return minDays;
+        return ans;
     }
 };
