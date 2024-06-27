ca65eb7982c77ee94b756b7d425f1dff814808f0
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-27 14:33:05 +0530

    Time: 67 ms (92.25%), Space: 46.4 MB (95.35%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -1,39 +1,24 @@
 class Solution {
+    vector<TreeNode*> sorted;
 public:
-    TreeNode* balanceBST(TreeNode* root) {
-        // Create a vector to store the inorder traversal of the BST
-        vector<int> inorder;
-        inorderTraversal(root, inorder);
-
-        int size = inorder.size();
-
-        // Construct and return the balanced BST
-        return createBalancedBST(inorder, 0, size - 1);
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        sorted.push_back(root);
+        inorder(root->right);
     }
-
-private:
-    void inorderTraversal(TreeNode* root, vector<int>& inorder) {
-        // Perform an inorder traversal to store the elements in sorted order
-        if (root == nullptr) return;
-        inorderTraversal(root->left, inorder);
-        inorder.push_back(root->val);
-        inorderTraversal(root->right, inorder);
-    }
-
-    TreeNode* createBalancedBST(const vector<int>& inorder, int start,
-                                int end) {
-        // Base case: if the start index is greater than the end index, return
-        // nullptr
+    
+    TreeNode* createTree(int start, int end) {
         if (start > end) return nullptr;
-
-        // Find the middle element of the current range
-        int mid = start + (end - start) / 2;
-
-        // Recursively construct the left and right subtrees
-        TreeNode* leftSubtree = createBalancedBST(inorder, start, mid - 1);
-        TreeNode* rightSubtree = createBalancedBST(inorder, mid + 1, end);
-
-        // Create a new node with the middle element and attach the subtrees
-        return new TreeNode(inorder[mid], leftSubtree, rightSubtree);
+        int mid = (end - start)/2 + start;
+        TreeNode *root = sorted[mid];
+        root->left = createTree(start, mid - 1);
+        root->right = createTree(mid + 1, end);
+        return root;
+        
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        inorder(root);
+        return createTree(0, sorted.size() - 1);
     }
 };
