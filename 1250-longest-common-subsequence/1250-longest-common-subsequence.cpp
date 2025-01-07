7a41bc8102947f84d457dd251ce55351d98654f9
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2025-01-07 13:02:47 +0530

    Time: 36 ms (28.36%), Space: 27.3 MB (52.96%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    string s,t;
-    int f( vector<vector<int>> &dp,int i,int j){
-        if(i>=s.size() || j>=t.size()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return 1+f(dp,i+1,j+1);
-        }
-        return dp[i][j]=max(f(dp,i+1,j),f(dp,i,j+1));
-    }
+    // string s,t;
+    // int f( vector<vector<int>> &dp,int i,int j){
+    //     if(i>=s.size() || j>=t.size()){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     if(s[i]==t[j]){
+    //         return 1+f(dp,i+1,j+1);
+    //     }
+    //     return dp[i][j]=max(f(dp,i+1,j),f(dp,i,j+1));
+    // }
     int longestCommonSubsequence(string s, string t) {
-        this->s=s,this->t=t;
         vector<vector<int>> dp(s.size(),vector<int>(t.size(),0));
         if(s[0]==t[0]){
             dp[0][0]=1;
@@ -31,6 +30,7 @@ public:
                 else{
                     if(i>0) dp[i][j]=max(dp[i][j],dp[i-1][j]);
                     if(j>0) dp[i][j]=max(dp[i][j],dp[i][j-1]);
+                    if(i>0 && j>0)dp[i][j]=max(dp[i][j],dp[i-1][j-1]);
                }
             }
         }
