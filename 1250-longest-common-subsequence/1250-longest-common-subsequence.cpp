9ed556e3a19186b5a07815c62158cb5068fc5f3e
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2025-03-09 18:36:18 +0530

    Time: 27 ms (56.72%), Space: 13 MB (80.22%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    // string s,t;
-    // int f( vector<vector<int>> &dp,int i,int j){
-    //     if(i>=s.size() || j>=t.size()){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     if(s[i]==t[j]){
-    //         return 1+f(dp,i+1,j+1);
-    //     }
-    //     return dp[i][j]=max(f(dp,i+1,j),f(dp,i,j+1));
-    // }
-    int longestCommonSubsequence(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),0));
-        if(s[0]==t[0]){
-            dp[0][0]=1;
-        }
-        int n=s.size(),m=t.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(s[i]==t[j]){
-                   if(i>0 && j>0) dp[i][j]=1+dp[i-1][j-1];
-                   else{
-                    dp[i][j]=1;
-                   }
-                }
-                else{
-                    if(i>0) dp[i][j]=max(dp[i][j],dp[i-1][j]);
-                    if(j>0) dp[i][j]=max(dp[i][j],dp[i][j-1]);
-                    if(i>0 && j>0)dp[i][j]=max(dp[i][j],dp[i-1][j-1]);
-               }
-            }
-        }
-        return dp[n-1][m-1];
-        // return f(dp,0,0);
+    int dp[1001][1001];
+    int lcs(int i, int j, string &a, string &b){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(a[i]==b[j]) return dp[i][j] = 1 + lcs(i-1,j-1,a,b);
+
+        return dp[i][j] = max(lcs(i,j-1,a,b),lcs(i-1,j,a,b));
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(dp,-1,sizeof(dp));
+        return lcs(text1.size()-1,text2.size()-1,text1,text2);
     }
 };
